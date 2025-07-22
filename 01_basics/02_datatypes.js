3ccbfe13bef1d28a6318d20e4180eb2609801c9c
Jyotsna Ray <ryjyotsna@gmail.com>
2025-07-22 18:44:36 +0530

    Added basic JavaScript files and variables

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all JS code as newer version
+alert("Hello World")//we are using nodejs,not browser, so no alert(used to display a simple alert box to the user)
+
+let name="John Doe"; //string
+let age=30; //number
+let isLoggedIn=true; //boolean
+
+//number=2 to power 53
+//bigint=2 to power 64
+//string="abc" or 'abc' or `abc`(template literals)
+//boolean=true or false
+//null=empty value,standalone value
+//undefined=variable not defined
+//symbol=unique value,not useful for beginners
+
+//object
+
+console.log(typeof null); //object (this is a bug in JS, null is not an object)
+console.log(typeof undefined); //undefined
